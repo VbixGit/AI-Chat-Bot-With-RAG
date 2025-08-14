b768ac68b89aada42397e531bff4f9c4a88ab716
Pratya.p VBix <pratya.p@vbix.co.th>
2025-08-14 06:50:39 +0000

    update front end

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { GeistSans } from "geist/font/sans";
 import "./globals.css";
-import { Toaster } from "@/components/ui/toaster"
+import { Toaster } from "@/components/ui/toaster";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,10 +18,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={`${inter.className} ${GeistSans.className}`}>
-        {children}
-        <Toaster />
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+          <Toaster />
+        </ThemeProvider>
       </body>
     </html>
   );
--- src/components/ui/icons.tsx
+++ src/components/ui/icons.tsx
@@ -90,3 +90,46 @@ export function IconStop(props: React.SVGProps<SVGSVGElement>) {
     </svg>
   );
 }
+
+export function IconSun(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      viewBox="0 0 16 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <circle cx="8" cy="8" r="4" />
+      <path d="M8 1.5V2.5" />
+      <path d="M8 13.5V14.5" />
+      <path d="M3.5 3.5L4.5 4.5" />
+      <path d="M11.5 11.5L12.5 12.5" />
+      <path d="M1.5 8H2.5" />
+      <path d="M13.5 8H14.5" />
+      <path d="M3.5 12.5L4.5 11.5" />
+      <path d="M11.5 4.5L12.5 3.5" />
+    </svg>
+  );
+}
+
+export function IconMoon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      viewBox="0 0 16 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <path d="M14.5 9.5C14.5 12.2614 12.2614 14.5 9.5 14.5C6.73858 14.5 4.5 12.2614 4.5 9.5C4.5 6.73858 6.73858 4.5 9.5 4.5C12.2614 4.5 14.5 6.73858 14.5 9.5Z" />
+      <path d="M9.5 1.5C9.5 2.05228 9.05228 2.5 8.5 2.5C7.94772 2.5 7.5 2.05228 7.5 1.5C7.5 0.947715 7.94772 0.5 8.5 0.5C9.05228 0.5 9.5 0.947715 9.5 1.5Z" />
+    </svg>
+  );
+}
